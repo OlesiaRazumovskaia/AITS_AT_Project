dd519d8f2e5bf4f670eae941ab56ee89767bec49
Разумовская Олеся <kizune2407@mail.ru>
2024-06-24 23:47:55 +0700

    Домашка по Java

---
+++ src/main/java/HomeworckOOP/DigitsNumbers.java
@@ -0,0 +1,40 @@
+package HomeworckOOP;
+
+import java.util.Scanner;
+
+public class DigitsNumbers {
+    public static void main(String[] args) {
+        int number;
+        int sum = 0;
+        int sumOdd = 0;
+        int max = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число:");
+        number = scanner.nextInt();
+
+        for (int i = number; i != 0; i /= 10) {
+
+            int digit = i % 10;
+
+            sum += digit;
+            if (checkOdd(digit)){
+                sumOdd += digit;
+            }
+
+            max = getMax(digit, max);
+        }
+
+        System.out.println("Сумма цифр введенного числа равна " + sum);
+        System.out.println("Сумму нечетных цифр введенного числа равна " + sumOdd);
+        System.out.println("Максимальная цифра числа равна " + max);
+    }
+
+    public static boolean checkOdd(int i) {
+        return i % 2 != 0;
+    }
+
+    public static int getMax (int a, int b) {
+        return Math.max(a, b);
+    }
+}
